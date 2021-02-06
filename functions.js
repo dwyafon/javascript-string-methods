37d4cf02f2549611e8094c8eba0d9d79a11cc8fd
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-02-06 11:22:09 +0000

    add string.prototype.charCodeAt()

--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
 const functions = {
-    stringCharAt: (string, index) => string.charAt(index) 
+    stringCharAt: (string, index) => string.charAt(index),
+    stringCharCodeAt: (string, index) => string.charCodeAt(index)
 }
 
 module.exports = functions
--- functions.test.js
+++ functions.test.js
@@ -4,3 +4,7 @@ const functions = require('./functions')
 test('should return the character at the given index', () => {
   expect(functions.stringCharAt('hjahsdkhasjdlkjahslkdjhaskjdh', 6)).toEqual('k')
 })
+
+test('should return an integer that represents the UTF-16 code unit for the character at the given index of the string', () => {
+  expect(functions.stringCharCodeAt('lkjsdlfjsljdlkfjljwkljl;kj;lkj', 18)).toEqual(119)
+})
