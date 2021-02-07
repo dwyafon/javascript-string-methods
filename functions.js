e89024e270054f7f4dce6ba26d709d98fe878108
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-02-07 17:23:32 +0000

    add String.prototype.codePointAt()

--- functions.js
+++ functions.js
@@ -1,6 +1,7 @@
 const functions = {
     stringCharAt: (string, index) => string.charAt(index),
-    stringCharCodeAt: (string, index) => string.charCodeAt(index)
+    stringCharCodeAt: (string, index) => string.charCodeAt(index),
+    stringCodePointAt: (string) => string.codePointAt(0)
 }
 
 module.exports = functions
--- functions.test.js
+++ functions.test.js
@@ -8,3 +8,7 @@ test('should return the character at the given index', () => {
 test('should return an integer that represents the UTF-16 code unit for the character at the given index of the string', () => {
   expect(functions.stringCharCodeAt('lkjsdlfjsljdlkfjljwkljl;kj;lkj', 18)).toEqual(119)
 })
+
+test('should return the UTF-16 code point for the passed string', () => {
+  expect(functions.stringCodePointAt('Ø')).toEqual(216)
+})
