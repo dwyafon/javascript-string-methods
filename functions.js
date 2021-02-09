e280a7a3f520a21f70713a71458c02b72ab95861
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-02-09 12:41:21 +0000

    add String.prototype.concat()

--- functions.js
+++ functions.js
@@ -1,7 +1,9 @@
 const functions = {
-    stringCharAt: (string, index) => string.charAt(index),
-    stringCharCodeAt: (string, index) => string.charCodeAt(index),
-    stringCodePointAt: (string) => string.codePointAt(0)
+  stringCharAt: (string, index) => string.charAt(index),
+  stringCharCodeAt: (string, index) => string.charCodeAt(index),
+  stringCodePointAt: (string) => string.codePointAt(0),
+  stringConcat: (string1, string2, string3) =>
+    string1.concat(' ', string2, ' ', string3),
 }
 
 module.exports = functions
--- functions.test.js
+++ functions.test.js
@@ -2,13 +2,21 @@ const { exportAllDeclaration } = require('@babel/types')
 const functions = require('./functions')
 
 test('should return the character at the given index', () => {
-  expect(functions.stringCharAt('hjahsdkhasjdlkjahslkdjhaskjdh', 6)).toEqual('k')
+  expect(functions.stringCharAt('hjahsdkhasjdlkjahslkdjhaskjdh', 6)).toEqual(
+    'k'
+  )
 })
 
 test('should return an integer that represents the UTF-16 code unit for the character at the given index of the string', () => {
-  expect(functions.stringCharCodeAt('lkjsdlfjsljdlkfjljwkljl;kj;lkj', 18)).toEqual(119)
+  expect(
+    functions.stringCharCodeAt('lkjsdlfjsljdlkfjljwkljl;kj;lkj', 18)
+  ).toEqual(119)
 })
 
 test('should return the UTF-16 code point for the passed string', () => {
   expect(functions.stringCodePointAt('Ø')).toEqual(216)
+})
+
+test('should return a string consisting of concated strings', () => {
+  expect(functions.stringConcat('I', 'love', 'you')).toEqual('I love you')
 })
